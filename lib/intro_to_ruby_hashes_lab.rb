7c1e516b0dcfd1f27b4c87dc533ff1d08d8f281b
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-26 20:13:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,8 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   new_hash = {}
-  key = :name
-  new_hash[key] = value
+  name = :key
+  new_hash[] = value
 end
 
 def read_from_hash(hash, key)
